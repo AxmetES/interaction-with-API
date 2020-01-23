06410888e82afea22e7f8e21278edd9c9584eba2
AxmetES <axmet-1984@mail.ru>
2020-01-23 18:25:21 +0600

    - In func "predict_salary" was changed comparison, from (if currency != 'rub' and currency != 'RUR': pass) to (if not currency == 'rub' and not currency == 'RUR': return) - Renaming 'average_all' to 'all_average_salary' , 'response_api' to 'api_response', 'average_all' to 'average_salary', "for name in names:" to "for language in languages:", 'VACANCY_TABLE' to 'vacancy_sj_table' and 'vacancy_hh_table' - Wrapped table creating process in loop

--- work_parser.py
+++ work_parser.py
@@ -6,66 +6,65 @@ import requests
 
 
 def predict_salary(salary_from, salary_to, currency):
-    if currency != 'rub' and currency != 'RUR':
-        pass
-    else:
-        if salary_from and salary_to is not None:
-            average_salary = (salary_from + salary_to) / 2
-            return average_salary
-        elif not salary_to:
-            average_salary = salary_from * 1.2
-            return average_salary
-        elif not salary_from:
-            average_salary = salary_to * 0.8
-            return average_salary
+    if not currency == 'rub' and not currency == 'RUR':
+        return
+    if salary_from and salary_to is not None:
+        average_salary = (salary_from + salary_to) / 2
+        return average_salary
+    elif not salary_to:
+        average_salary = salary_from * 1.2
+        return average_salary
+    elif not salary_from:
+        average_salary = salary_to * 0.8
+        return average_salary
 
 
 def get_sj_api(url, headers, name):
-    average_all = []
+    all_average_salary = []
     per_page = 0
     for page in count(0):
         response = requests.get(url=url, headers=headers,
                                 params={'keyword': name, 'count': 20, 'town': 4, 'page': page})
         response.raise_for_status()
-        response_api = response.json()
-        pages = response_api['total'] / 20
+        api_response = response.json()
+        pages = api_response['total'] / 20
         per_page += 1
         if page >= pages:
             break
 
-        for item in response_api['objects']:
-            average_all.append(predict_salary(item['payment_from'], item['payment_to'], item['currency']))
+        for item in api_response['objects']:
+            all_average_salary.append(predict_salary(item['payment_from'], item['payment_to'], item['currency']))
 
-    average_salary = sum(average_all) / len(average_all)
+    average_salary = sum(all_average_salary) / len(all_average_salary)
 
-    vacancy_data = {"vacancies_found": response_api['total'],
-                    "vacancies_processed": len(average_all),
+    vacancy_data = {"vacancies_found": api_response['total'],
+                    "vacancies_processed": len(all_average_salary),
                     "average_salary": int(average_salary)}
     return vacancy_data
 
 
 def get_hh_api(base_url, headers, name):
     vacancies = []
-    average_all = []
+    average_salary = []
     for page in count(0):
         page_response = requests.get(url=base_url, headers=headers,
                                      params={'text': name, 'area': 1, 'per_page': 100, 'period': 30,
                                              'page': page})
         page_response.raise_for_status()
-        page_data = page_response.json()
+        api_response = page_response.json()
         if page >= 19:
             break
-        vacancies.append(page_data['items'])
+        vacancies.append(api_response['items'])
 
     for vacancy in vacancies:
         for item in vacancy:
             if item['salary'] is not None:
-                average_all.append(predict_salary(item['salary']['from'], item['salary']['to'],
+                average_salary.append(predict_salary(item['salary']['from'], item['salary']['to'],
                                                      item['salary']['currency']))
-    average_clear = list(filter(None, average_all))
-    average_all = sum(average_clear) / len(average_all)
-    vacancy_data = {"vacancies_found": page_data['found'], "vacancies_processed": len(average_all),
-                    "average_salary": int(average_all)}
+    average_clear = list(filter(None, average_salary))
+    all_average_salary = sum(average_clear) / len(average_salary)
+    vacancy_data = {"vacancies_found": api_response['found'], "vacancies_processed": len(average_salary),
+                    "average_salary": int(all_average_salary)}
     return vacancy_data
 
 
@@ -77,63 +76,30 @@ def main():
     headers_sj = {'X-Api-App-Id': secret_key}
     headers_hh = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.117 Safari/537.36'}
-    names = 'python', 'c', 'c#', 'c++', 'java', 'javascript', 'ruby', 'go', '1c'
+    languages = ['python', 'c', 'c#', 'c++', 'java', 'javascript', 'ruby', 'go', '1c']
     direct_sj = {}
     direct_hh = {}
 
-    for name in names:
-        direct_sj[name] = get_sj_api(base_url_sj, headers_sj, name)
-        direct_hh[name] = get_hh_api(base_url_hh, headers_hh, name)
+    vacancy_sj_table = [['Язык программирования', 'Вакансий найдено', 'Вакансий обработано', 'Средняя зарплата']]
+    vacancy_hh_table = [['Язык программирования', 'Вакансий найдено', 'Вакансий обработано', 'Средняя зарплата']]
 
-    title = 'SuperJob Moscow'
-    VACANCY_TABLE = [
-        ['Язык программирования', 'Вакансий найдено', 'Вакансий обработано', 'Средняя зарплата'],
-        ['python', direct_sj['python']['vacancies_found'], direct_sj['python']['vacancies_processed'],
-         direct_sj['python']['average_salary']],
-        ['c', direct_sj['c']['vacancies_found'], direct_sj['c']['vacancies_processed'],
-         direct_sj['c']['average_salary']],
-        ['c#', direct_sj['c#']['vacancies_found'], direct_sj['c#']['vacancies_processed'],
-         direct_sj['c#']['average_salary']],
-        ['c++', direct_sj['c++']['vacancies_found'], direct_sj['c++']['vacancies_processed'],
-         direct_sj['c++']['average_salary']],
-        ['java', direct_sj['java']['vacancies_found'], direct_sj['java']['vacancies_processed'],
-         direct_sj['java']['average_salary']],
-        ['javascript', direct_sj['javascript']['vacancies_found'], direct_sj['javascript']['vacancies_processed'],
-         direct_sj['javascript']['average_salary']],
-        ['ruby', direct_sj['ruby']['vacancies_found'], direct_sj['ruby']['vacancies_processed'],
-         direct_sj['ruby']['average_salary']],
-        ['go', direct_sj['go']['vacancies_found'], direct_sj['go']['vacancies_processed'],
-         direct_sj['go']['average_salary']],
-        ['1c', direct_sj['1c']['vacancies_found'], direct_sj['1c']['vacancies_processed'],
-         direct_sj['1c']['average_salary']],
-    ]
-    table_instance = AsciiTable(VACANCY_TABLE, title)
+    for language in languages:
+        direct_sj[language] = get_sj_api(base_url_sj, headers_sj, language)
+        vacancy_sj_table.append(
+            [language, direct_sj[language]['vacancies_found'], direct_sj[language]['vacancies_processed'],
+             direct_sj[language]['average_salary']])
+        direct_hh[language] = get_hh_api(base_url_hh, headers_hh, language)
+        vacancy_hh_table.append(
+            [language, direct_hh[language]['vacancies_found'], direct_hh[language]['vacancies_processed'],
+             direct_hh[language]['average_salary']])
+
+    sj_title = 'SuperJob Moscow'
+    hh_title = 'Headhunter Moscow'
+
+    table_instance = AsciiTable(vacancy_sj_table, sj_title)
     print(table_instance.table)
 
-    title = 'Headhunter Moscow'
-    VACANCY_TABLE = [
-        ['Язык программирования', 'Вакансий найдено', 'Вакансий обработано', 'Средняя зарплата'],
-        ['python', direct_hh['python']['vacancies_found'], direct_hh['python']['vacancies_processed'],
-         direct_hh['python']['average_salary']],
-        ['c', direct_hh['c']['vacancies_found'], direct_hh['c']['vacancies_processed'],
-         direct_hh['c']['average_salary']],
-        ['c#', direct_hh['c#']['vacancies_found'], direct_hh['c#']['vacancies_processed'],
-         direct_hh['c#']['average_salary']],
-        ['c++', direct_hh['c++']['vacancies_found'], direct_hh['c++']['vacancies_processed'],
-         direct_hh['c++']['average_salary']],
-        ['java', direct_hh['java']['vacancies_found'], direct_hh['java']['vacancies_processed'],
-         direct_hh['java']['average_salary']],
-        ['javascript', direct_hh['javascript']['vacancies_found'], direct_hh['javascript']['vacancies_processed'],
-         direct_hh['javascript']['average_salary']],
-        ['ruby', direct_hh['ruby']['vacancies_found'], direct_hh['ruby']['vacancies_processed'],
-         direct_hh['ruby']['average_salary']],
-        ['go', direct_hh['go']['vacancies_found'], direct_hh['go']['vacancies_processed'],
-         direct_hh['go']['average_salary']],
-        ['1c', direct_hh['1c']['vacancies_found'], direct_hh['1c']['vacancies_processed'],
-         direct_hh['1c']['average_salary']],
-    ]
-
-    table_instance = AsciiTable(VACANCY_TABLE, title)
+    table_instance = AsciiTable(vacancy_hh_table, hh_title)
     print(table_instance.table)
 
 
